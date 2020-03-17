1fb003f525ec0a015bd30021eebe607e8b3bbb6e
Alexandru Branza <branza.alex@gmail.com>
2020-03-17 09:49:43 +0200

    Add Channel Up / Down and Change Channel

--- smartthingstv/smartthingstv/api.py
+++ smartthingstv/smartthingstv/api.py
@@ -84,9 +84,20 @@ class smartthingstv:
             cmdurl = requests.post(API_COMMAND,data=COMMAND_UNMUTE ,headers=REQUEST_HEADERS)
       elif cmdtype == "switch": # turns off
          cmdurl = requests.post(API_COMMAND,data=COMMAND_POWER_OFF ,headers=REQUEST_HEADERS)
-      elif cmdtype == "selectsource": #changes source
+      elif cmdtype == "selectsource": # changes source
          API_COMMAND_DATA =  "{'commands':[{'component': 'main','capability': 'mediaInputSource','command': 'setInputSource', 'arguments': "
          API_COMMAND_ARG  = "['{}']}}]}}".format(command)
          API_FULL = API_COMMAND_DATA + API_COMMAND_ARG
          cmdurl = requests.post(API_COMMAND,data=API_FULL ,headers=REQUEST_HEADERS)
-
+      elif cmdtype == "selectchannel": # changes channel
+         API_COMMAND_DATA =  "{'commands':[{'component': 'main','capability': 'tvChannel','command': 'setTvChannel', 'arguments': "
+         API_COMMAND_ARG  = "['{}']}}]}}".format(command)
+         API_FULL = API_COMMAND_DATA + API_COMMAND_ARG
+         cmdurl = requests.post(API_COMMAND,data=API_FULL ,headers=REQUEST_HEADERS)
+      elif cmdtype == "stepchannel": # steps channel up or down
+         if command == "up":
+            API_COMMAND_DATA = "{'commands':[{'component': 'main','capability': 'tvChannel','command': 'channelUp'}]}"
+            cmdurl = requests.post(API_COMMAND,data=API_COMMAND_DATA ,headers=REQUEST_HEADERS)
+         else:
+            API_COMMAND_DATA = "{'commands':[{'component': 'main','capability': 'tvChannel','command': 'channelDown'}]}"
+            cmdurl = requests.post(API_COMMAND,data=API_COMMAND_DATA ,headers=REQUEST_HEADERS)
